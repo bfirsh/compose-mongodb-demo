e8ae5345aaa4fc5a289219295acb28ac1801f52d
Ben Firshman <ben@firshman.co.uk>
2015-06-20 22:15:04 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from pymongo import MongoClient
+import os
+import datetime
+
+app = Flask(__name__)
+mongo = MongoClient(host=os.environ.get('MONGO_HOST', 'mongo'), port=27017)
+db = mongo['test']
+
+@app.route('/')
+def hello():
+    db.hits.insert({"ip": request.remote_addr, "ts": datetime.datetime.utcnow()})
+    return 'Hello World! I have been seen by %s.' % db.hits.count()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
